cfaaf9b4ed53f93e94e2b814c938f09cbac5e239
Alexey Fedorov <mail@alexeyfv.ru>
2025-04-06 22:52:38 +0200

    Add OG image generator

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,28 +1,30 @@
 import { defineCollection, z } from 'astro:content'
 
-// 60 characters
-// With Google's Title update, shorter titles are preferred.
-// If the title is too long, they are more likely to be rewritten.
-// Use these rules of thumb when it comes to the title tag
-// length: Maximum length: 60 characters or 575 pixels.2
-
 const blog = defineCollection({
   // Type-check frontmatter using a schema
-  schema: ({ image }) =>
-    {
-      return z.object({
-        title: z.string().max(60),
-        description: z.string(),
-        // Transform string to Date object
-        pubDate: z
-          .string()
-          .or(z.date())
-          .transform((val) => new Date(val)),
-        heroImage: image(),
-        tags: z.array(z.string()),
-        lang: z.string()
-      })
-    },
+  schema: ({ image }) => {
+    return z.object({
+      // 60 characters
+      // With Google's Title update, shorter titles are preferred.
+      // If the title is too long, they are more likely to be rewritten.
+      // Use these rules of thumb when it comes to the title tag
+      // length: Maximum length: 60 characters or 575 pixels.2
+      title: z.string().max(60),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z
+        .string()
+        .or(z.date())
+        .transform((val) => new Date(val)),
+      // cover is optional
+      // if not provided, it will be generated
+      cover: z.optional(image()),
+      // Tags are mandatory
+      tags: z.array(z.string()),
+      // Language is mandatory
+      lang: z.string(),
+    })
+  },
 })
 
 export const collections = { blog }
---
+++ src/utils/cover.ts
@@ -0,0 +1,149 @@
+import { Resvg } from '@resvg/resvg-js'
+import satori from 'satori'
+import fs from 'fs'
+import path from 'path'
+
+const defaultRatio = {
+  width: 1536,
+  height: 1024,
+}
+
+export function getCover(props): ImageMetadata {
+  const cover = props.data.cover
+
+  // If the cover is provided, use it
+  if (cover) {
+    return cover
+  }
+
+  // id = lang\path-to-post\index.mdx
+  const id = props.id as string
+  const slashIndex = id.lastIndexOf('/')
+  const slug = id.substring(0, slashIndex)
+
+  // Otherwise, use the generated image
+  const generated: ImageMetadata = {
+    src: `post/${slug}/cover.png`,
+    width: defaultRatio.width,
+    height: defaultRatio.height,
+    format: 'png',
+  }
+
+  return generated
+}
+
+export async function generateCover(title: string) {
+  const fontFamily = 'Ubuntu+Mono'
+  const fontWeight = 400
+  const fontStyle = 'normal'
+
+  const data = await getGoogleFont(fontFamily, fontWeight)
+
+  const element = createOgImage(title)
+
+  const svg = await satori(element, {
+    width: defaultRatio.width,
+    height: defaultRatio.height,
+    embedFont: true,
+    fonts: [
+      {
+        name: fontFamily,
+        style: fontStyle,
+        weight: fontWeight,
+        data: data,
+      },
+    ],
+  })
+
+  const resvg = new Resvg(svg)
+
+  const pngData = resvg.render()
+
+  return pngData.asPng()
+}
+
+function createOgImage(title: string): any {
+  const imagePath = path.resolve('./src/assets/images/background.png')
+  const imageBuffer = fs.readFileSync(imagePath)
+  const base64Image = `data:image/png;base64,${imageBuffer.toString('base64')}`
+
+  return {
+    type: 'img',
+    props: {
+      src: base64Image,
+      style: {
+        width: defaultRatio.width,
+        height: defaultRatio.height,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: '#fff',
+        textAlign: 'center',
+        position: 'relative',
+      },
+      children: [
+        {
+          type: 'div',
+          props: {
+            style: {
+              backgroundColor: 'rgba(0, 0, 0, 0.8)',
+              borderRadius: '16px',
+              padding: '40px 60px',
+              maxWidth: '1300px',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              gap: '80px',
+            },
+            children: [
+              {
+                type: 'div',
+                props: {
+                  style: {
+                    fontSize: 64,
+                  },
+                  children: title,
+                },
+              },
+              {
+                type: 'div',
+                props: {
+                  style: {
+                    fontSize: 40,
+                  },
+                  children: 'alexeyfv.xyz',
+                },
+              },
+            ],
+          },
+        },
+      ],
+    },
+  }
+}
+
+async function getGoogleFont(
+  font: string,
+  weight: number,
+): Promise<ArrayBuffer> {
+  const API = `https://fonts.googleapis.com/css2?family=${font}:wght@${weight}`
+  const response = await fetch(API)
+  const text = await response.text()
+
+  const resource = text.match(
+    /src: url\((.+?)\) format\('(opentype|truetype)'\)/,
+  )
+
+  if (!resource) {
+    throw new Error('Failed to download dynamic font')
+  }
+
+  const res = await fetch(resource[1])
+
+  if (!res.ok) {
+    throw new Error('Failed to download dynamic font. Status: ' + res.status)
+  }
+
+  return res.arrayBuffer()
+}
